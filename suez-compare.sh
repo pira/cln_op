522943a940474d0879cfbd9e03a7a8a8761ca176
Borsche <borsche@borsche-hfsp.com>
2022-06-24 23:22:18 +0200

    Update with latest link creation

--- suez-compare.sh
+++ suez-compare.sh
@@ -5,26 +5,31 @@
 #
 SUEZ_DIR="$HOME/suez"
 STATUS_DIR='statuses'
-TEMP_STATUS='curr.status'
 stamp=`date '+%Y-%m-%d-%H%M%S'`
 
 cd $SUEZ_DIR
-/usr/bin/poetry run ./suez --show-chan-ids --client=c-lightning | awk 'BEGIN { FPAT = "([[:space:]]*[[:alnum:][:punct:][:digit:]]+)"; OFS = ""; } { $6=$7=$8=$9="";  print $0; }' >$TEMP_STATUS
+/usr/bin/poetry run ./suez --show-chan-ids --client=c-lightning | awk 'BEGIN { FPAT = "([[:space:]]*[[:alnum:][:punct:][:digit:]]+)"; OFS = ""; } { $6=$7=$8=$9="";  print $0; }' >temp.status
 
 if [ -d $STATUS_DIR ];
 then
-	prev_status=`ls -1t $STATUS_DIR | head -1`
-	echo "Comparing current to $prev_status"
-	sort -k 7 $TEMP_STATUS >/tmp/suez.status.1
-	sort -k 7 $STATUS_DIR/$prev_status >/tmp/suez.status.2
-	result=`diff -U 0 /tmp/suez.status.2 /tmp/suez.status.1`
-	echo "$result"
+	prev_status=`readlink -f $STATUS_DIR/latest.status`
+	if [ -f $prev_status ];
+	then
+		echo "Comparing current to $prev_status"
+		sort -k 7 temp.status >/tmp/suez.status.1
+		sort -k 7 $prev_status >/tmp/suez.status.2
+		result=`diff -U 0 /tmp/suez.status.2 /tmp/suez.status.1`
+		echo "$result"
+	else
+		echo "Empty $STATUS_DIR, will populate"
+	fi
 else
 	echo "Creating $STATUS_DIR"
-	mkdir $STATUS_DIR
+	mkdir -p $STATUS_DIR
 fi
 
 echo "Creating $STATUS_DIR/$stamp.status"
-mv $TEMP_STATUS $STATUS_DIR/$stamp.status
+mv temp.status $STATUS_DIR/$stamp.status
+ln -fs ./$stamp.status $STATUS_DIR/latest.status
 
 echo "All done."
