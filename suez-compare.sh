b6fd6535623daa5feb461aafc566fef557aac6af
Borsche <borsche@borsche-hfsp.com>
2022-10-26 08:25:53 +0000

    htlc_max management and some initil seeding for compare

--- suez-compare.sh
+++ suez-compare.sh
@@ -10,18 +10,30 @@ stamp=`date '+%Y-%m-%d-%H%M%S'`
 cd $SUEZ_DIR
 /usr/bin/poetry run ./suez --show-chan-ids --client=c-lightning | awk 'BEGIN { FPAT = "([[:space:]]*[[:alnum:][:punct:][:digit:]]+)"; OFS = ""; } { $6=$7=$8=$9="";  print $0; }' >temp.status
 
+# Compare two given status files and display result. Recent first
+fn_compare() {
+	sort -k 7 $1 >/tmp/suez.status.1
+	sort -k 7 $2 >/tmp/suez.status.2
+	result=`diff -w -U 0 /tmp/suez.status.2 /tmp/suez.status.1`
+	echo "$result"
+}
+
 if [ -d $STATUS_DIR ];
 then
 	prev_status=`readlink -f $STATUS_DIR/latest.status`
 	if [ -f $prev_status ];
 	then
 		echo "Comparing current to $prev_status"
-		sort -k 7 temp.status >/tmp/suez.status.1
-		sort -k 7 $prev_status >/tmp/suez.status.2
-		result=`diff -w -U 0 /tmp/suez.status.2 /tmp/suez.status.1`
-		echo "$result"
+		fn_compare temp.status $prev_status
 	else
-		echo "Empty $STATUS_DIR, will populate"
+		prev_status=$STATUS_DIR/`ls -1t $STATUS_DIR | head -1`
+		if [ -f $prev_status ];
+		then
+			echo "latest.status missing, will compare to $prev_status"
+			fn_compare temp.status $prev_status			
+		else
+			echo "Empty $STATUS_DIR, nothing to compare, will populate"
+		fi
 	fi
 else
 	echo "Creating $STATUS_DIR"
