8b587db5d0e8c75c5257cd22d76ee215a05ab320
Borsche <borsche@borsche-hfsp.com>
2022-11-12 15:48:26 -0600

    encrypted hsm

--- start-ln.sh
+++ start-ln.sh
@@ -3,7 +3,7 @@
 # restart tor if hang up
 #sudo service tor --full-restart
 
-#lightningd --daemon --log-file /var/log/lightning/lightning-filtered-debug.log --log-level=debug:channeld
-#lightningd --daemon --log-file /var/log/lightning/lightning.log --plugin=/home/borsche/circular/circular --circular-peer-refresh=600
-lightningd --daemon --log-file /var/log/lightning/lightning.log
+#lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning-filtered-debug.log --log-level=debug:channeld
+#lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning.log --plugin=/home/borsche/circular/circular --circular-peer-refresh=600
+lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning.log
 
