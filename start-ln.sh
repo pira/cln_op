7c91468b428afb807b84778abecfae7abc6832df
Borsche <borsche@borsche-hfsp.com>
2022-11-14 09:43:44 +0000

    field sizing

--- start-ln.sh
+++ start-ln.sh
@@ -4,6 +4,6 @@
 #sudo service tor --full-restart
 
 #lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning-filtered-debug.log --log-level=debug:channeld
-#lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning.log --plugin=/home/borsche/circular/circular --circular-peer-refresh=600
-lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning.log
+lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning.log --plugin=/home/borsche/circular/circular --circular-peer-refresh=600
+#lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning.log
 
