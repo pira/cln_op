066515ed6931e97aa2e3deb121aa2e2628d68ccd
Borsche <borsche@borsche-hfsp.com>
2022-12-30 01:05:20 +0000

    db upgrade flag

--- start-ln.sh
+++ start-ln.sh
@@ -4,6 +4,6 @@
 #sudo service tor --full-restart
 
 #lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning-filtered-debug.log --log-level=debug:channeld
-lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning.log --plugin=/home/borsche/plugins/circular --circular-peer-refresh=600
+lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning.log --plugin=/home/borsche/plugins/circular --circular-peer-refresh=600 --database-upgrade=true
 #lightningd --encrypted-hsm --daemon --log-file /var/log/lightning/lightning.log
 
