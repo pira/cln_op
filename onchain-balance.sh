c81742282dfd5401d7ef08bb8b31df86773096a2
Borsche <borsche@borsche-hfsp.com>
2023-08-15 23:19:32 -0500

    fix awk scientific notation for small amounts

--- onchain-balance.sh
+++ onchain-balance.sh
@@ -2,7 +2,7 @@
 
 # Just parses listfunds output to display available onchain balance"
 #
-funds_msat=`lightning-cli listfunds | jq '.outputs[] | select (.reserved == false) | select(.status == "confirmed") | .amount_msat' | awk '{s+=$1} END {print s}'`
+funds_msat=`lightning-cli listfunds | jq '.outputs[] | select (.reserved == false) | select(.status == "confirmed") | .amount_msat' | awk '{s+=$1} END {printf "%.0f",s}'`
 funds=`echo $((funds_msat / 1000)) | numfmt --g`
 
 echo $funds "sats"
